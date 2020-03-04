d2f1b2ba1f48f307bca3863ec929b50651f421c6
Mr. Mueller <bmueller@hstat.org>
2020-03-04 22:08:26 +0000

    ggd entry

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Mr. Mueller"
+let userName = "bmuellerhstat";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
